987a9d4ec3b0ccfc67ea24e13a16c983853c4161
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-24 21:06:07 +0900

    Update example.test.cpp

--- example.test.cpp
+++ example.test.cpp
@@ -1,23 +1,9 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B"
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
-#define ALL(x) std::begin(x), std::end(x)
+#define PROBLEM "https://atcoder.jp/contests/practice/tasks/practice_1"
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n; cin >> n;
-    vector<int> s(n);
-    REP (i, n) {
-        cin >> s[i];
-    }
-    int q; cin >> q;
-    int cnt = 0;
-    while (q --) {
-        int t_i; cin >> t_i;
-        cnt += binary_search(ALL(s), t_i);
-    }
-    cout << cnt << endl;
-    return 0;
+int main(){
+    int a, b, c; cin >> a >> b >> c;
+    string s; cin >> s;
+    cout << a + b + c << " " << s << endl;
 }
