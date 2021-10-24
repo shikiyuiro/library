50b9bec5666c9b9d3206f19c3a28dafd1f332930
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-24 21:51:59 +0900

    Delete example.test.cpp

--- example.test.cpp
+++
@@ -1,9 +0,0 @@
-#define PROBLEM "https://atcoder.jp/contests/practice/tasks/practice_1"
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int a, b, c; cin >> a >> b >> c;
-    string s; cin >> s;
-    cout << a + b + c << " " << s << endl;
-}
