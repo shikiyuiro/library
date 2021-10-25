92306c469e9bdb534c32761b858cd0d5b277b24f
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-25 16:59:43 +0900

    Update UnionFind.cpp

--- Data Structure/UnionFind.cpp
+++ Data Structure/UnionFind.cpp
@@ -1,6 +1,3 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
-#include<bits/stdc++.h>
-using namespace std;
 
 class Union_find{
 public:
@@ -10,14 +7,3 @@ public:
     void unite(long x, long y){ par[root(x)] = root(y); }
     bool same(long x, long y){ return root(x) == root(y); }
 };
-
-int main(){
-    long N, Q; cin >> N >> Q;
-    Union_find uf(N);
-    long t, u, v;
-    while(Q--){
-        cin >> t >> u >> v;
-        if(t == 0) uf.unite(u, v);
-        if(t == 1) cout << (uf.same(u, v) ? 1 : 0) << endl;
-    }
-}
