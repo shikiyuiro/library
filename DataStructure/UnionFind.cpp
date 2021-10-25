6e253134596e1b68e79be9ac5ade62f83e0ae467
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-25 17:19:38 +0900

    Create UnionFind.cpp

---
+++ DataStructure/UnionFind.cpp
@@ -0,0 +1,9 @@
+
+class Union_find{
+public:
+    vector<long> par;
+    Union_find(long size){ par.resize(size); for(long i = 0; i < size; i++) par[i] = i; }
+    long root(long x){ return (par[x] == x) ? x: par[x] = root(par[x]); }
+    void unite(long x, long y){ par[root(x)] = root(y); }
+    bool same(long x, long y){ return root(x) == root(y); }
+};
