5ff429294169a6bdc88cf9c799f41780579e8595
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-26 23:20:28 +0900

    Update SparseTable.test.cpp

--- test/verify/SparseTable.test.cpp
+++ test/verify/SparseTable.test.cpp
@@ -9,7 +9,7 @@ int main(){
     vector<long> A(N);
     for(long i = 0; i < N; i++) cin >> A[i];
     
-    SparseTable STA(A, INT_MAX, [](long a, long b){return min(a, b);});
+    SparseTable<> STA(A, INT_MAX, [](long a, long b){return min(a, b);});
     while(Q--){
         long u, v; cin >> u >> v;
         cout << STA.query(u, v) << endl;
