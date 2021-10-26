ac0f339946e7dd8eeba60d7fadcf46667be2a463
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-26 16:15:33 +0900

    Create SparseTable.test.cpp

---
+++ test/verify/SparseTable.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include "../../template/template.cpp"
+
+#include "../../DataStructure/SparseTable.cpp"
+
+int main(){
+    long N, Q; cin >> N >> Q;
+    vector<long> A(N);
+    for(long i = 0; i < N; i++) cin >> A[i];
+    
+    SparseTable STA(A, INT_MAX, [](long a, long b){return min(a, b);});
+    while(Q--){
+        long u, v; cin >> u >> v;
+        cout << STA.query(u, v) << endl;
+    }
+}
