4adece48d1bbfc9bad4b6afe801b1e053f1d34e4
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-27 11:09:01 +0900

    Update UnionFind.cpp

--- DataStructure/UnionFind.cpp
+++ DataStructure/UnionFind.cpp
@@ -5,7 +5,12 @@ private:
 public:
     UnionFind(long size){ Root.resize(size); iota(Root.begin(), Root.end(), 0); Size.resize(size, 1);}
     long find(long x){ return (Root[x] == x) ? x: Root[x] = find(Root[x]); }
-    void unite(long x, long y){ if(Size[find(x)] < Size[find(y)]) swap(x, y); Root[find(y)] = find(x); Size[find(x)] += Size[find(y)];}
+    void unite(long x, long y){
+        if(find(x) == find(y)) return;
+        if(Size[find(x)] < Size[find(y)]) swap(x, y);
+        Root[find(y)] = find(x);
+        Size[find(x)] += Size[find(y)];
+    }
     bool same(long x, long y){ return find(x) == find(y);}
     long size(long x){ return Size[find(x)];}
 };
