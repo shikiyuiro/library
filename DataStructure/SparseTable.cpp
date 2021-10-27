425bc0158ad81d835e721b24126d8e73b6b56fc4
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-27 15:47:50 +0900

    Update SparseTable.cpp

--- DataStructure/SparseTable.cpp
+++ DataStructure/SparseTable.cpp
@@ -1,17 +1,17 @@
-template < typename T = long >
+template < typename Element = long >
 class SparseTable{
 private:
-    function<T(T, T)> opera;
+    function<Element(Element, Element)> opera;
 public:
-    vector<vector<T>> table;
+    vector<vector<Element>> table;
     vector<long> cf;
     
-    SparseTable(vector<T>& v, T e, function<T(T, T)> operation) {
+    SparseTable(vector<Element>& v, Element e, function<Element(Element, Element)> operation) {
         opera = operation;
         long isiz = v.size();
         long jsiz = 0;
         while((1 << jsiz) <= isiz) jsiz++;
-        table.resize(isiz, vector<T>(jsiz, e));
+        table.resize(isiz, vector<Element>(jsiz, e));
         for(long i = 0; i < isiz; i++)table[i][0] = v[i];
         for(long j = 1; j < jsiz; j++){
             for(long i = 0; i + (1 << (j - 1)) < isiz; i++){
@@ -22,7 +22,7 @@ public:
         for(long i = 2; i <= isiz; i++) cf[i] = cf[i >> 1] + 1;
     }
     
-    T query(long l, long r/*半開区間*/){
+    Element query(long l, long r/*半開区間*/){
         assert(l < r);
         long b = cf[r - l];
         return opera(table[l][b], table[r - (1 << b)][b]);
