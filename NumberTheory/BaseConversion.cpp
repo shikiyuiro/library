534836da81da963a7aad15ebdcb6c319ff60f3c3
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-27 13:22:37 +0900

    Create BaseConversion.cpp

---
+++ NumberTheory/BaseConversion.cpp
@@ -0,0 +1,27 @@
+class BaseConversion {
+private:
+    string symbol = "0123456789ABCDEF";
+    long to_num[128];
+public:
+    BaseConversion(){
+        long basemax = symbol.length();
+        for(long i = 0; i < basemax; i++) to_num[(long)symbol[i]] = i;
+    }
+    
+    string from10(long num, long base) {
+        if(num == 0) return "0";
+        string res = "";
+        while(num > 0){
+            res = symbol[num % base] + res;
+            num /= base;
+        }
+        return res;
+    }
+    
+    long to10(string num, long base) {
+        long res = 0;
+        long numsize = num.length();
+        for(long i = 0; i < numsize; ++i) res = res * base + to_num[(long)num[i]];
+        return res;
+    }
+};
