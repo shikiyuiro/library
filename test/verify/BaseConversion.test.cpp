9bfeaf61fd3ce671a0713c4834c819d9c53ac0a2
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-27 14:17:32 +0900

    Create BaseConversion.test.cpp

---
+++ test/verify/BaseConversion.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0306
+
+#include "../../template/template.cpp"
+
+#include "../../NumberTheory/BaseConversion.cpp"
+
+int main(){
+    BaseConversion bc;
+    long w; cin >> w;
+    long t = 1;
+    while(w >= t){
+        t *= 3;
+        t += 1;
+    }
+    string S = bc.from10(w + t, 3);
+    long siz = S.size();
+    for(long i = 0; i < siz; i++){
+        if(S[i] == '2') cout << "+";
+        if(S[i] == '1') cout << "0";
+        if(S[i] == '0') cout << "-";
+    }
+}
