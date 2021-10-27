d607b39a97a655ad21d7cbf99a0328f2bd775327
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-27 14:18:49 +0900

    Update BaseConversion.test.cpp

--- test/verify/BaseConversion.test.cpp
+++ test/verify/BaseConversion.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0306
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0306"
 
 #include "../../template/template.cpp"
 
