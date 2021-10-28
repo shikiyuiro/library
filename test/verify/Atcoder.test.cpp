8ba6baba0069cdfc86844365ef1ee6c1edf2dd84
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-10-28 19:18:38 +0900

    Create Atcoder.test.cpp

---
+++ test/verify/Atcoder.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "../../template/template.cpp"
+
+#include "../../Utility/CoordinateCompless.cpp"
+//以下の問題で動作確認済みです。
+//https://atcoder.jp/contests/abc036/submissions/26855886
+
+int main(){
+    long A, B; cin >> A >> B;
+    cout << A + B << endl;
+}
