5668b4df1bf78e39543c80dce2e8765f91e823c5
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-11-06 17:18:06 +0900

    Update Atcoder.test.cpp

--- test/verify/Atcoder.test.cpp
+++ test/verify/Atcoder.test.cpp
@@ -6,6 +6,10 @@
 //以下の問題で動作確認済みです。
 //https://atcoder.jp/contests/abc036/submissions/26855886
 
+#include "../../DataStructure/LazySegmentTree.cpp"
+//以下の問題で動作確認済みです。
+//https://atcoder.jp/contests/typical90/submissions/27052694
+
 int main(){
     long A, B; cin >> A >> B;
     cout << A + B << endl;
