d6efd4c2390d78e96b35405839f726ae959daa65
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2021-12-22 19:45:44 +0900

    Update LowestCommonAncestor.cpp

--- Graph/LowestCommonAncestor.cpp
+++ Graph/LowestCommonAncestor.cpp
@@ -1,57 +1,29 @@
 #include "../DataStructure/SparseTable.cpp"
 
 class LowestCommonAncestor{
-private:
-    long N;
-    vector<bool> visited;
-    vector<vector<long> > Tree;
-    vector<vector<long> > STtable;
-    vector<long> STcf;
-    
-    void make_EularTour(long vis){
-        EularTour.push_back(depth[vis] * N + vis);
-        visited[vis] = true;
-        for(auto e : Tree[vis]){
-            if(visited[e]) continue;
-            depth[e] = depth[vis] + 1;
-            make_EularTour(e);
-            EularTour.push_back(depth[vis] * N + vis);
-        }
-    }
-    
-    void make_SparseTable(){
-        SparseTable<long> st(EularTour, LONG_MAX, [](long a, long b){return min(a, b);});
-        STtable = st.table;
-        STcf = st.cf;
-    }
-    
-    void make_first(){
-        long index = 0;
-        for(auto e : EularTour){
-            first_index[e % N] = min(first_index[e % N], index);
-            index++;
-        }
-    }
 public:
-    vector<long> depth;
-    vector<long> EularTour;
-    vector<long> first_index;
-    
-    LowestCommonAncestor(vector<vector<long>>& tree, long root) : Tree(tree){
-        N = Tree.size();
-        visited.resize(N, false);
-        depth.resize(N, 0);
-        first_index.resize(N, INT_MAX);
-        make_EularTour(root);
-        make_SparseTable();
-        make_first();
+    LowestCommonAncestor(vector<vector<long>> &tree, long root) : tree(tree){
+        ETpos.resize(tree.size(), -1);
+        dfs(root, 0);
+        st = SparseTable<long>(EularTour, LONG_MAX, [](long a, long b){return min(a, b);});
     }
     
-    long query(long l, long r){
-        long ltmp = l, rtmp = r;
-        l = min(first_index[ltmp], first_index[rtmp]); r = max(first_index[ltmp], first_index[rtmp]) + 1;
-        long b = STcf[r - l];
-        return min(STtable[l][b], STtable[r - (1 << b)][b]) % N;
+    long query(long L, long R){
+        if(ETpos[L] > ETpos[R]) swap(L, R);
+        return st.query(ETpos[L], ETpos[R] + 1) % tree.size();
+    }
+    
+private:
+    vector<vector<long>> tree;
+    vector<long> EularTour;
+    vector<long> ETpos;
+    long ETsize = 0;
+    SparseTable<long> st;
+    
+    void dfs(long vis, long depth){
+        EularTour.push_back(depth * tree.size() + vis);
+        ETpos[vis] = ETsize++;
+        for(auto e : tree[vis]) if(ETpos[e] == -1) {dfs(e, depth + 1); EularTour.push_back(depth * tree.size() + vis); ETsize++;}
     }
 };
 /**
