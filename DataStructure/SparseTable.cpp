b9a6695b87c6925721d3f1657447826c541df09f
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2022-01-17 10:14:58 +0900

    Update SparseTable.cpp

--- DataStructure/SparseTable.cpp
+++ DataStructure/SparseTable.cpp
@@ -1,14 +1,17 @@
-template < typename elm >
+template<class S, S (*op)(S, S), S (*e)()>
 class SparseTable{
 public:
+    vector<vector<S>> table;
+    vector<long> cf;
+    
     explicit SparseTable() = default;
     
-    SparseTable(vector<elm>& v, elm e, function<elm(elm, elm)> op) : op(op){
+    SparseTable(vector<S>& v){
         long isiz = v.size();
         long jsiz = 0;
         while((1 << jsiz) <= isiz) jsiz++;
-        table.resize(isiz, vector<elm>(jsiz, e));
-        for(long i = 0; i < isiz; i++)table[i][0] = v[i];
+        table.resize(isiz, vector<S>(jsiz, e()));
+        for(long i = 0; i < isiz; i++) table[i][0] = v[i];
         for(long j = 1; j < jsiz; j++){
             for(long i = 0; i + (1 << (j - 1)) < isiz; i++){
                 table[i][j] = op(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
@@ -18,15 +21,11 @@ public:
         for(long i = 2; i <= isiz; i++) cf[i] = cf[i >> 1] + 1;
     }
     
-    elm query(long L, long R/*[L, R)*/){
-        assert(L < R);
-        long b = cf[R - L];
-        return op(table[L][b], table[R - (1 << b)][b]);
+    S query(long l, long r/*半開区間*/){
+        if(l == r) return e();
+        long b = cf[r - l];
+        return op(table[l][b], table[r - (1 << b)][b]);
     }
-private:
-    function<elm(elm, elm)> op;
-    vector<vector<elm>> table;
-    vector<long> cf;
 };
 /**
  * @brief スパーステーブル
