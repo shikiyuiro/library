ee6f94cad21c6c04f0537ee9455096ef4b0c26fd
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2022-01-17 10:34:33 +0900

    Update SparseTable.test.cpp

--- test/verify/SparseTable.test.cpp
+++ test/verify/SparseTable.test.cpp
@@ -4,12 +4,15 @@
 
 #include "../../DataStructure/SparseTable.cpp"
 
+long op(long a, long b){ return min(a, b);}
+long e() { return LONG_MAX;}
+
 int main(){
     long N, Q; cin >> N >> Q;
     vector<long> A(N);
     for(long i = 0; i < N; i++) cin >> A[i];
     
-    SparseTable<long> STA(A, LONG_MAX, [](long a, long b){return min(a, b);});
+    SparseTable<long, op, e> STA(A);
     while(Q--){
         long u, v; cin >> u >> v;
         cout << STA.query(u, v) << endl;
