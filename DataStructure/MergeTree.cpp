e96dc051f930c85f66cd03bf90aaee06fa301b73
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2022-01-28 15:07:45 +0900

    Create MergeTree.cpp

---
+++ DataStructure/MergeTree.cpp
@@ -0,0 +1,44 @@
+#include "../DataStructure/UnionFind_PartiallyPersistent.cpp"
+
+class MergeTree{
+    //Union-Findのマージ過程を表す木。  
+    //「時刻tにおける要素uの連結成分」に対するクエリを列クエリに帰着できる。  
+    //発想はHL分解の部分木に対するクエリに近い。
+public:
+    MergeTree(long siz, vector<tuple<long,long,long>> merge){
+        uni = PPUF(siz);
+        forest.resize(siz);
+        pos.resize(siz);
+        for(auto [u, v, t] : merge){
+            long fu = uni.find(u, t), fv = uni.find(v, t);
+            if(uni.unite(fu, fv, t)){
+                forest[fu].push_back(fv);
+                forest[fv].push_back(fu);
+            }
+        }
+        vector<bool> visited(siz, false);
+        for(long v = 0; v < siz; v++){
+            if(visited[v]) continue;
+            long fv = uni.find(v, INT_MAX);
+            auto dfs = [&](auto &&self, long vis, long prev) -> void {
+                visited[vis] = true;
+                tour.push_back(vis);
+                pos[vis] = tour.size() - 1;
+                for(auto e : forest[vis]) if(e != prev) self(self, e, vis);
+            };
+            dfs(dfs, fv, -1);
+        }
+    }
+    vector<long> get_tour(){
+        return tour;
+    }
+    pair<long,long> get_range(long u, long t){
+        long L = pos[uni.find(u, t)];
+        return {L, L + uni.size(u, t)};
+    }
+private:
+    vector<vector<long>> forest;
+    vector<long> tour;
+    vector<long> pos;
+    PPUF uni;
+};
