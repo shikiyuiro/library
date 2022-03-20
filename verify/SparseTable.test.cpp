665b6a4bfe92b16832793bedfab93539f73829dc
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2022-03-20 13:25:55 +0900

    Create SparseTable.test.cpp

---
+++ verify/SparseTable.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include "../../template/template.cpp"
+#include "../../DataStructure/SparseTable.cpp"
+
+int64_t op(int64_t a, int64_t b){
+    return min(a, b);
+}
+
+int64_t e(){
+    return INT64_MAX;
+}
+
+int main(){
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
+    int64_t N, Q; cin >> N >> Q;
+    vector<int64_t> A(N);
+    for(int64_t i = 0; i < N; i++) cin >> A[i];
+    SparseTable<int64_t, op, e> sp(A);
+    while(Q--){
+        int64_t l, r; cin >> l >> r;
+        cout << sp.fold(l, r) << '\n';
+    }
+}
+
