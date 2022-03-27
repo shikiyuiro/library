9e3c62fb353d93a14b02b0f39329aafb926194df
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2022-03-27 20:12:14 +0900

    Create Inversion.test.cpp

---
+++ verify/Inversion.test.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "../template/template.cpp"
+#include "../Classic/Inversion.cpp"
+//以下の問題で動作確認済みです。
+//https://atcoder.jp/contests/chokudai_S001/submissions/30509909
+
+int main(){
+    int64_t A, B; cin >> A >> B;
+    cout << A + B << '\n';
+}
