f80b28b1d3662f034498a5e87ce2b85dd0e4c54c
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2022-03-29 16:57:51 +0900

    Create RMQ_ON_O1.cpp

---
+++ DataStructure/RMQ_ON_O1.cpp
@@ -0,0 +1,51 @@
+template<class S, S (*op)(S, S), S (*e)()>
+class RMQ_ON_O1{
+    const int32_t Bsiz = 5;
+    const int32_t Bcmpl = 31;
+public:
+    explicit RMQ_ON_O1() = default;
+    RMQ_ON_O1(vector<S> &_v) : v(_v){
+        v.resize(((v.size()>>Bsiz)+1)<<Bsiz, e());
+        b.resize(v.size());
+        for(int32_t i = 0; i < (int32_t)v.size(); i++){
+            stack<S> st;
+            while(true){
+                while(not st.empty()){
+                    if(op(v[i], v[st.top()]) != v[i]) break;
+                    st.pop();
+                }
+                b[i] = st.empty() ? 0 : b[st.top()] | (1 << st.top());
+                if((i & Bcmpl) == Bcmpl) break;
+                st.push(i++);
+            }
+            v_mini.push_back(fold_mini(i-Bcmpl,i+1));
+        }
+        sp = SparseTable<S,op,e>(v_mini);
+    }
+    S fold(int32_t L, int32_t R){
+        int32_t Lu = (L & ~Bcmpl) + Bcmpl + 1;
+        int32_t Rd = (R & ~Bcmpl);
+        if(Lu > Rd) return fold_mini(L, R);
+        S res = e();
+        res = op(res, fold_mini(L, Lu));
+        res = op(res, sp.fold(Lu>>Bsiz, Rd>>Bsiz));
+        res = op(res, fold_mini(Rd, R));
+        return res;
+    }
+private:
+    vector<S> v;
+    vector<S> v_mini;
+    vector<int32_t> b;
+    SparseTable<S,op,e> sp;
+    S fold_mini(int32_t L, int32_t R){
+        if(L == R) return e();
+        int32_t masked = (b[R-1] >> (L & Bcmpl)) << (L & Bcmpl);
+        int32_t lsb = __builtin_ctzll(masked);
+        if(masked == 0) return v[R-1];
+        else return v[lsb + (L & ~Bcmpl)];
+    }
+};
+/**
+ * @brief ( 構築$\mathrm{O}(N)$・クエリ$\mathrm{O}(1)$ )
+ * @docs docs/DataStructure/RMQ_ON_O1.md
+ */
