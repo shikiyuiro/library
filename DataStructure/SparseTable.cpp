333c31fde36fb927faeafc61d12be0663c70b833
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2022-03-29 17:04:29 +0900

    Update SparseTable.cpp

--- DataStructure/SparseTable.cpp
+++ DataStructure/SparseTable.cpp
@@ -4,24 +4,24 @@ public:
     explicit SparseTable() = default;
     SparseTable(vector<S>& v){
         isiz = v.size();
-        jsiz = 64 - __builtin_clzll(isiz);
+        jsiz = 32 - __builtin_clz(isiz);
         table.resize(isiz * jsiz, e());
-        for(int64_t i = 0; i < isiz; i++) table[i] = v[i];
-        for(int64_t j = 1; j < jsiz; j++){
-            for(int64_t i = 0; i + (1 << (j - 1)) < isiz; i++){
+        for(int32_t i = 0; i < isiz; i++) table[i] = v[i];
+        for(int32_t j = 1; j < jsiz; j++){
+            for(int32_t i = 0; i + (1 << (j - 1)) < isiz; i++){
                 table[i + isiz * j] = op(table[i + isiz * (j - 1)], table[i + (1 << (j - 1)) + isiz * (j - 1)]);
             }
         }
     }
-    S fold(int64_t L, int64_t R){
+    S fold(int32_t L, int32_t R){
         if(L == R) return e();
-        int64_t b = 64 - __builtin_clzll(R - L) - 1;
+        int32_t b = 32 - __builtin_clz(R - L) - 1;
         return op(table[L + isiz * b], table[R - (1 << b) + isiz * b]);
     }
 private:
     vector<S> table;
-    int64_t isiz;
-    int64_t jsiz;
+    int32_t isiz;
+    int32_t jsiz;
 };
 /**
  * @brief スパーステーブル
